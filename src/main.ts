0fe0505297a986fcfc825f4a0a1a99d3fc0b218c
miniBear <ax_zaa@windowslive.com>
2026-08-26 11:47:35 +0700

    Fix: Enable graceful application shutdown
    This allows the application to perform cleanup operations when receiving termination signals (e.g., SIGTERM), enhancing stability during restarts or deployments.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { GlobalExceptionFilter } from './utils/filters/global-exception-filter'
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule)
 
+  app.enableShutdownHooks()
   app.use(cookieParser())
   app.use(json({ limit: '50mb' }))
   app.use(urlencoded({ limit: '50mb', extended: true }))
